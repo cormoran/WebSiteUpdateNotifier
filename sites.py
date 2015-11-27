4e5f6c52f59b78f5b0069334b1796673458d2e7d
cormoran <cormoran707@gmail.com>
2015-11-27 09:06:14 +0900

    情報知能工学科休講掲示板追加

--- sites.py
+++ sites.py
@@ -4,52 +4,71 @@
 import re
 from update_checker import *
 
+
 class KuCs(UpdateChecker):
 
     def __init__(self):
         name = '神戸大学情報知能工学科教学掲示板'
-        url  = 'http://www.csi.kobe-u.ac.jp/cs/site/student/kyougaku.html'
-        UpdateChecker.__init__(self,name,url)
+        url = 'http://www.csi.kobe-u.ac.jp/cs/site/student/kyougaku.html'
+        UpdateChecker.__init__(self, name, url)
 
-        
-    def parse(self,html):
+    def parse(self, html):
         head = '<textarea name="textarea" cols="90" rows="15">'
         tail = '</textarea>'
         st = html.find(head) + len(head)
         html = html[st:]
-        ed  = html.find(tail)
+        ed = html.find(tail)
         html = html[:ed]
         return html
 
-    
-    def beautify(self,parsed):
+    def beautify(self, parsed):
         return parsed
 
 
-    
 class KuCommon(UpdateChecker):
+
     def __init__(self):
         name = '神戸大学国際教養教育院'
-        url  = 'http://www.iphe.kobe-u.ac.jp/zengaku.htm#'
-        UpdateChecker.__init__(self,name,url)
+        url = 'http://www.iphe.kobe-u.ac.jp/zengaku.htm#'
+        UpdateChecker.__init__(self, name, url)
 
-        
-    def parse(self,html):
+    def parse(self, html):
         head = '<article class="news">'
         tail = '</article>'
         st = html.find(head) + len(head)
         html = html[st:]
-        ed  = html.find(tail)
+        ed = html.find(tail)
         html = html[:ed]
 
-        
         return html
-    
-    def beautify(self,parsed):
 
-        matches = re.findall('<dt>(.*[^>])</dt>.*[^<]<a.*[^>]>(.*[^<])</a>',parsed)
-        ret =''
+    def beautify(self, parsed):
+
+        matches = re.findall(
+            '<dt>(.*[^>])</dt>.*[^<]<a.*[^>]>(.*[^<])</a>', parsed)
+        ret = ''
         for match in matches:
             ret += match[0] + ' ' + match[1] + '\r\n'
-        
+
         return ret
+
+
+class KuCsKyuko(UpdateChecker):
+
+    def __init__(self):
+        name = '情報知能工学科休講掲示板'
+        url = 'http://www.csi.kobe-u.ac.jp/cs/site/student/kyuukou-hoko.html'
+        UpdateChecker.__init__(self, name, url)
+
+    def parse(self, html):
+        head = '<!-- contents -->'
+        tail = '<!-- /contents -->'
+        st = html.find(head) + len(head)
+        html = html[st:]
+        ed = html.find(tail)
+        html = html[:ed]
+
+        return html
+
+    def beautify(self, parsed):
+        return parsed
